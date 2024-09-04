c5f49e22e5a4d881647b63cf81666b82769681b3
Andreas Bresser <andreas.bresser@dfki.de>
2024-09-04 16:39:03 +0200

    re-introduce rclpy locally to run a synchronous test

--- integration_tests/launch_testing/com_launch_test.py
+++ integration_tests/launch_testing/com_launch_test.py
@@ -3,14 +3,9 @@
 # launch_testing_ros/test/examples/check_msgs_launch_test.py
 
 # Python basics to launch helloric_ui_com
-import subprocess
-
+import asyncio
 from threading import Event
 from threading import Thread
-
-# Websocket-client library to test connection
-import json
-import websocket
 
 # ROS 2 basics
 import rclpy
@@ -19,15 +14,18 @@ from std_msgs.msg import Bool, Int8
 # ROS 2 launchfile
 import launch
 import launch.actions
-import launch_ros.actions
 import launch_testing.actions
+import launch_testing.markers
 
 # unit testing and pytest
 import pytest
 import unittest
 
-# launch_testing
-import launch_testing.markers
+# see https://fastapi.tiangolo.com/advanced/testing-websockets/
+from fastapi.testclient import TestClient
+
+# custom init_websocket to get app that we use for mocking
+from helloric_ui_com.main import init_websocket
 
 
 @pytest.mark.launch_test
@@ -38,81 +36,59 @@ def generate_test_description():
     ])
 
 
-
 class TestFixture(unittest.TestCase):
-    def spin(self):
-        try:
-            while rclpy.ok() and not self.spinning.is_set():
-                rclpy.spin_once(self.node, timeout_sec=0.1)
-        finally:
-            pass
+    async def _spin(self):
+        while rclpy.ok() and not self.spinning.is_set():
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+            await self.mgr.update_data()
 
-    def start_helloric_ui_com(self):
-        return
+    def spin_thread_wrapper(self):
+        """a thread to run a coroutine.
+        
+        We need to call asyncio-broadcast that requires async."""
+        self.ros_spin_loop.run_until_complete(self._spin())
+
 
     def setup_ros(self):
-        rclpy.init()
-        self.node = rclpy.create_node('test_node')
+        self.spinning = Event()
+        self.clients = []
+        mgr, app = init_websocket()
+        self.app = app
+        self.mgr = mgr
+        self.node = mgr.ros_node
 
         self.publisher_emo = self.node.create_publisher(
             Int8, 'llm/speech/emotion', 10)
         self.publisher_speak = self.node.create_publisher(
             Bool, 'llm/speech/speaking', 10)
 
-        self.spinning = Event()
-        # Add a spin thread
-        self.ros_spin_thread = Thread(target=self.spin)
-        self.ros_spin_thread.start()
-
     def setUp(self):
         self.setup_ros()
-
-        # start server
-        self.websocket_response = Event()
-        self.helloric_ui_com = subprocess.Popen(
-            ['helloric_ui_com', '--host', '127.0.0.1', '--port', '18260'])
+        self.spinning = Event()
+        self.ros_spin_loop = asyncio.new_event_loop()
+        self.ros_spin_thread = Thread(target=self.spin_thread_wrapper)
+        self.ros_spin_thread.start()
 
     def tearDown(self):
-        if self.helloric_ui_com:
-            self.helloric_ui_com.kill()
         self.spinning.set()
         self.ros_spin_thread.join()
         self.node.destroy_node()
         rclpy.shutdown()
 
-
-    def receive_websocket(self):
-        return json.loads(self.ws.recv())
-
     def test_websocket_called(self):
         """the only test: make sure that the websocket has been executed."""
-        
-        # TODO: this should be a ROS node that send a message and receives
-        # the emotion from the given websocket
-        
-        uri = "ws://localhost:18260/ws"
-        print(f'connecting to {uri}')
-        self.ws = websocket.WebSocket()
-        connected = False
-        while not connected:
-            try:
-                self.ws.connect(uri)
-                connected = True
-            except ConnectionRefusedError:
-                pass
-        # we did not send anything yet, the websocket message should be 0 first
-        ws_msg = self.receive_websocket()
-        assert 'emotion' in ws_msg
-        assert ws_msg['emotion'] == 0
+        client = TestClient(self.app)
+        with client.websocket_connect("/ws") as websocket:
+            # we did not send anything yet, the websocket message should be 0 first
+            ws_msg = websocket.receive_json()
+            assert 'emotion' in ws_msg
+            assert ws_msg['emotion'] == 0
 
-        # if we send 5 we should receive 5
-        msg = Int8()
-        msg.data = 5
-        self.publisher_emo.publish(msg)
+            # if we send 5 we should receive 5
+            msg = Int8()
+            msg.data = 5
+            self.publisher_emo.publish(msg)
 
-        ws_msg = self.receive_websocket()
-        assert 'emotion' in ws_msg
-        assert ws_msg['emotion'] == 5
-
-
-
+            ws_msg = websocket.receive_json()
+            assert 'emotion' in ws_msg
+            assert ws_msg['emotion'] == 5
